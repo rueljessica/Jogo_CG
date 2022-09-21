0bef90593c9439134e0d422fbf949ff287643247
rueljessica <andreia_ruel@hotmail.com>
2022-09-21 20:06:35 -0300

    Funcionando

---
+++ Assets/Scripts/BulletPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPlayer : Bullet {
+
+    protected override void Awake() {
+        base.Awake();
+    }
+
+    protected override void Start() {
+        base.Start();
+    }
+
+    protected override void FixedUpdate() {
+        base.FixedUpdate();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+} 
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    [SerializeField] private GameObject shooter;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+
+
+    public void shoot()
+    {
+        if(bulletPrefab != null && firePoint != null && shooter != null)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity) as GameObject;
+            Bullet bulletComponent = bullet.GetComponent<Bullet>();
+            if(shooter.transform.localScale.x < 0)
+            {
+                bulletComponent.direction = Vector2.left;
+            } else {
+                bulletComponent.direction = Vector2.right;
+            }
+        }
+    }
+}
