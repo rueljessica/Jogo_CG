9a299cfa31cf4974ecd1aa6257b566b6210f2149
merhisolins <92954553+merhisolins@users.noreply.github.com>
2022-09-22 05:26:28 -0300

    adicionei os cenarios e animação de tiro, mas o cenario ta com um bug na fisica que não consegui resolver, ele acontece quando inicia o jogo

---
+++ jogo/Assets/Atirar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atirar : MonoBehaviour
+{
+    [SerializeField] private Transform _posiçãoTiro;
+    [SerializeField] private GameObject _tiroPrefab;
+
+    private void Atira(){
+        Instantiate(_tiroPrefab, _posiçãoTiro.position, _posiçãoTiro.rotation);
+
+    }
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2")){
+            Atira();
+        }
+    }
+}
---
+++ jogo/Assets/ProjetioBala.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+     [RequireComponent(typeof(Rigidbody2D))]
+public class ProjetioBala : MonoBehaviour
+{
+    [SerializeField] private float _velocidade; 
+    void Start()
+    {
+        var rb = GetComponent<Rigidbody2D>();
+        rb.velocity = rb.transform.right * _velocidade;
+    }
+    
+    private void OnTriggerEnter2D(ColliderDistance2D collison)
+    {
+        Destroy(gameObject);
+    }
+
+}
